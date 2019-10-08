a98d06772c10ad0dbf7b331ca933b84fe6770ffe
JorgeGiovannetti <giovannetti2000@gmail.com>
2019-10-08 11:49:23 -0500

    Click events simplified and values stored in array

--- content.js
+++ content.js
@@ -1,31 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  var mono = false;
+  var eqBands = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+  function updateSliders(sliderValue, sliderId){
+    eqBands[parseInt(sliderId.substr(4),10)-1] = sliderValue;
+    console.log(eqBands);
+  }
+
+
+  Array.from(document.getElementsByClassName('slider')).forEach(function(element) {
+    //Call upon array update onclick  
+    element.addEventListener('click', function(e){updateSliders(e.target.value, e.target.id);});
+    //Reset sliders on double click
+    element.addEventListener('dblclick', function(e){e.target.value = 0;});
+  });
+  
+
   //Reset band sliders
   document.getElementById('reset').onclick = function () {
     Array.from(document.getElementsByClassName("band")).forEach((bSlider) => {
       bSlider.value = 0;
+      updateSliders(bSlider.value, bSlider.id);
     });
     console.log("RESET!");
   };
-  //Reset sliders on double click
-  document.getElementById('band1').ondblclick = function () {band1.value = 0;}
-  document.getElementById('band2').ondblclick = function () {band2.value = 0;}
-  document.getElementById('band3').ondblclick = function () {band3.value = 0;}
-  document.getElementById('band4').ondblclick = function () {band4.value = 0;}
-  document.getElementById('band5').ondblclick = function () {band5.value = 0;}
-  document.getElementById('band6').ondblclick = function () {band6.value = 0;}
-  document.getElementById('band7').ondblclick = function () {band7.value = 0;}
-  document.getElementById('band8').ondblclick = function () {band8.value = 0;}
-  document.getElementById('band9').ondblclick = function () {band9.value = 0;}
-  document.getElementById('band10').ondblclick = function () {band10.value = 0;}
-  document.getElementById('band11').ondblclick = function () {band11.value = 0;}
-  document.getElementById('band12').ondblclick = function () {band12.value = 0;}
-
-  //Mono toggle (incomplete)
-  document.getElementById('mono').onclick = function () {
-    Array.from(document.getElementsByClassName("band")).forEach((bSlider) => {
-      console.log("The value of " + bSlider.id + " is " + bSlider.value);
   
-    });
-    console.log("Mono");
+  //Mono toggle
+  document.getElementById('radio-a').onclick = function() {mono = true;}
+  document.getElementById('radio-b').onclick = function() {mono = false;}
+  
+  document.getElementById('mono').onclick = function () {
+    if(mono){
+      console.log("Mono");
+    } else console.log("Stereo");
   };
+
+
 }, false);
