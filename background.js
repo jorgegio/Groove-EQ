cb25ee396e022b58b1b3d2cce0f2f92cf3186128
JorgeGiovannetti <giovannetti2000@gmail.com>
2019-10-09 11:13:30 -0500

    Added messaging between background and content

--- background.js
+++ background.js
@@ -1,9 +1,20 @@
 console.log("Background running");
+var eqBands = [0,0,0,0,0,0,0,0,0,0,0,0];
 
-chrome.browserAction.onClicked.addListener(buttonClicked);
 
-function buttonClicked(tab) {
-  console.log("button clicked");
+chrome.runtime.onMessage.addListener(function (element) {
+  if (element == "getValues"){
+    console.log("Sending values... beep boop");
+    chrome.runtime.sendMessage({type: "bandValues", value: eqBands});
+  }
+  if (element.type) {
+    eqBands[parseInt(element.type.substr(4),10)-1] = parseInt(element.value);
+    console.log("Listened for values: ", eqBands);
+  }
+});
+
+function updateEq(tab) {
+  console.log("Update Eq Message Sent");
   let msg = {
     txt: "hello"
   }
