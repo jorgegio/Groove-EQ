b67f8d6eaceae3ba0316a127a2742f5fae297a7c
JorgeGiovannetti <giovannetti2000@gmail.com>
2019-10-09 13:27:17 -0500

    Added gain listeners and fixed mono messages

--- background.js
+++ background.js
@@ -1,22 +1,25 @@
 console.log("Background running");
 var eqBands = [0,0,0,0,0,0,0,0,0,0,0,0];
-
+var mono = false;
+var gain = 0;
 
 chrome.runtime.onMessage.addListener(function (element) {
   if (element == "getValues"){
     console.log("Sending values... beep boop");
     chrome.runtime.sendMessage({type: "bandValues", value: eqBands});
+    chrome.runtime.sendMessage({type: "monoValue", value: mono});
+    chrome.runtime.sendMessage({type: "gainValue", value: gain});
   }
-  if (element.type) {
+  if(element.type) {
     eqBands[parseInt(element.type.substr(4),10)-1] = parseInt(element.value);
     console.log("Listened for values: ", eqBands);
   }
-});
-
-function updateEq(tab) {
-  console.log("Update Eq Message Sent");
-  let msg = {
-    txt: "hello"
+  if(element.type == "mono"){
+    mono = element.value;
+    console.log("Background received mono");
   }
-  chrome.tabs.sendMessage(tab.id, msg);
-}
+  if(element.type == "gain"){
+    gain = element.value;
+    console.log("Background received gain");
+  }
+});
