747e4f58e1ed59e3e84cc4b3d35e5daf5bde6f0a
JorgeGiovannetti <giovannetti2000@gmail.com>
2019-10-11 15:17:33 -0500

    Added audiostream capture (bugfixing pending)

--- background.js
+++ background.js
@@ -1,25 +1,33 @@
-console.log("Background running");
 var eqBands = [0,0,0,0,0,0,0,0,0,0,0,0];
+[30,60,90,160,300,500,900,1600,3000,5000,9000,16000]
 var mono = false;
 var gain = 0;
+const context = new window.AudioContext;
+
+function getTabStream(){
+  chrome.tabCapture.capture({audio: true, video: false}, (stream) => {
+    console.log(stream);
+    const sourceNode = context.createMediaStreamSource(stream);
+    const eq = context.createGain();
+    sourceNode.connect(eq);
+    eq.connect(context.destination);
+  });
+}
 
 chrome.runtime.onMessage.addListener(function (element) {
   if (element == "getValues"){
-    console.log("Sending values... beep boop");
+    getTabStream();
     chrome.runtime.sendMessage({type: "bandValues", value: eqBands});
     chrome.runtime.sendMessage({type: "monoValue", value: mono});
     chrome.runtime.sendMessage({type: "gainValue", value: gain});
   }
   if(element.type) {
     eqBands[parseInt(element.type.substr(4),10)-1] = parseInt(element.value);
-    console.log("Listened for values: ", eqBands);
   }
   if(element.type == "mono"){
     mono = element.value;
-    console.log("Background received mono");
   }
   if(element.type == "gain"){
     gain = element.value;
-    console.log("Background received gain");
   }
 });
