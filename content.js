b450676c187d66d7afd42238d3c764a8e5aa3130
JorgeGiovannetti <giovannetti2000@gmail.com>
2019-10-14 23:25:28 -0500

    Added gain band and its functionality

--- content.js
+++ content.js
@@ -3,9 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     chrome.runtime.sendMessage("popupOpened");
   }
 
-
   var mono = false;
-  var gain = 1;
 
   function updateSliders(sliderValue, sliderId){
     chrome.runtime.sendMessage({type: sliderId, value: document.getElementById(sliderId).value});
@@ -19,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     chrome.runtime.sendMessage({type: "mono", value: mono});
   }
   function updateGain(){
-    chrome.runtime.sendMessage({type: "gain", value: gain});
+    chrome.runtime.sendMessage({type: "gain", value: document.getElementById('gain').value});
   }
 
   chrome.runtime.onMessage.addListener(function (element) {
@@ -39,33 +37,43 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     }
     if(element.type == "gainValue"){
-      gain = element.value;
+      document.getElementById('gain').value = element.value;
     }
   });
 
+  document.getElementById('gain').addEventListener('click',function(e){updateGain(e.value,e.id)})
 
   Array.from(document.getElementsByClassName('slider')).forEach(function(element) {
     //Call upon array update onclick  
     element.addEventListener('click', function(e){
-      if(e.target.id){
-        updateSliders(e.target.value, e.target.id);
-      }
+      if(element.id !='gain'){
+        if(e.target.id){
+          updateSliders(e.target.value, e.target.id);
+        }
+      } else updateGain();
     });
     //Reset sliders on double click
     element.addEventListener('dblclick', function(e){
-      e.target.value = 0;
-      updateSliders(e.target.value, e.target.id);
+      if(e.target.id !='gain'){
+        e.target.value = 0;
+        updateSliders(e.target.value, e.target.id);
+      } else {
+        e.target.value = 1;
+        updateGain();
+      }
     });
   });
   
 
   //Reset band sliders
   document.getElementById('reset').onclick = function () {
-    Array.from(document.getElementsByClassName("band")).forEach((bSlider) => {
+    Array.from(document.getElementsByClassName('band')).forEach((bSlider) => {
       bSlider.value = 0;
       updateSliders(bSlider.value, bSlider.id);
     });
     mono = false;
+    document.getElementById('gain').value = 1;
+    updateGain();
     updateMono();
     chrome.runtime.sendMessage("reset");
     console.log("RESET!");
